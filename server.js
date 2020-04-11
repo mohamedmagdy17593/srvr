98f1b9d7a21e2113f9f343d7002e03a10ac3e8de
Mohamed Magdy <mohamed_magdy_17593@outlook.com>
2020-04-11 17:51:52 +0200

    add views and middlewares

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const srvr = require('./lib/srvr')
+const path = require('path')
 
 const app = srvr()
 
-app.get('/', (req, res) => {
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'pug')
+
+function logger(req, res, next) {
+  console.log(req.url)
+  next()
+}
+app.use(logger)
+
+app.get('/', (_req, res) => {
   res.send('hello root')
 })
 
-app.listen(3000)
+app.get('/view', (_req, res) => {
+  res.render('index', { name: 'Mohamed' })
+})
+
+app.listen(1234)
